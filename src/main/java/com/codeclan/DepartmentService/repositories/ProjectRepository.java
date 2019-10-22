f60c9f064e0dc9ac4e31e4aad30339d0b34ad5c0
Michael Alexander <michael_alexander_20@outlook.com>
2019-10-22 11:12:17 +0100

    Created interfaces. Woo.

---
+++ src/main/java/com/codeclan/DepartmentService/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.codeclan.DepartmentService.repositories;
+
+import com.codeclan.DepartmentService.models.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
