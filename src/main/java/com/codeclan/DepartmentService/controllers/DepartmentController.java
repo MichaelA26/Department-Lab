bb957cf35f0aff3fe414517cc1ea5aff81698cc9
Michael Alexander <michael_alexander_20@outlook.com>
2019-10-22 11:20:32 +0100

    Setup controllers

---
+++ src/main/java/com/codeclan/DepartmentService/controllers/DepartmentController.java
@@ -0,0 +1,23 @@
+package com.codeclan.DepartmentService.controllers;
+
+import com.codeclan.DepartmentService.models.Department;
+import com.codeclan.DepartmentService.repositories.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/departments")
+public class DepartmentController {
+
+    @Autowired
+    DepartmentRepository departmentRepository;
+
+    @GetMapping
+    public List<Department> getAllDepartments(){
+        return departmentRepository.findAll();
+    }
+}
---
+++ src/main/java/com/codeclan/DepartmentService/controllers/ProjectController.java
@@ -0,0 +1,23 @@
+package com.codeclan.DepartmentService.controllers;
+
+import com.codeclan.DepartmentService.models.Project;
+import com.codeclan.DepartmentService.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/projects")
+public class ProjectController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @GetMapping
+    public List<Project> getAllProjects(){
+        return projectRepository.findAll();
+    }
+}
